aac7fd979dc123f1f1db3b68b11dd4d9b3829f28
Suraj20 <bhosalesuraj2012@gmail.com>
2025-07-06 12:58:18 +0530

    data ingestion notbook added

--- src/textSummarizer/utils/common.py
+++ src/textSummarizer/utils/common.py
@@ -11,14 +11,14 @@ from typing import Any
 @ensure_annotations
 def read_yaml(path_to_yaml:Path)->ConfigBox:
   try:
-    with open(path_to_yaml) as yaml_file:
-      content=yaml.safe_load(yaml_file)
-      logger.info(f"yaml file: {path_to_yaml} loaded successfully")
-      return ConfigBox(content)
+      with open(path_to_yaml) as yaml_file:
+        content = yaml.safe_load(yaml_file)
+        logger.info(f"yaml file: {path_to_yaml} loaded successfully")
+        return ConfigBox(content)
   except BoxValueError:
-    raise ValueError("yaml file is empty")
+        raise ValueError("yaml file is empty")
   except Exception as e:
-    raise e 
+        raise e
   
 
 @ensure_annotations
